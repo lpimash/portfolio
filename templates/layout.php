4fabfdd8ede2ca965099dafbe32481595b8b4f1e
o_revollat <revollat@gmail.com>
2016-10-19 15:13:51 +0000

    ok

--- templates/layout.php
+++ templates/layout.php
@@ -3,10 +3,11 @@
 <head>
   <meta charset="utf-8">
   <title><?=$this->e($title)?></title>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
     
-    <?=$this->section('content')?>
+    <?php echo $this->section('content') ?>
   
 </body>
 </html>
